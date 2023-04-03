1ac3e60ecaed3027ded50ae9a65d1b2329fb01ea
EKTEST <e_zhitnikov@bk.ru>
2023-04-03 13:38:06 +0400

    added user progress

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -43,20 +43,24 @@ class ViewController: UIViewController {
         let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer{
-            print("Right!")
+            sender.backgroundColor = .green
         } else {
-            print("Wrong!")
+            sender.backgroundColor = .red
         }
         if questionNumber < quiz.count - 1 {
             questionNumber += 1
         } else {
             questionNumber = 0
         }
-        updateUI()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target:self, selector: #selector(updateUI), userInfo:nil, repeats: false)
     }
     
-    func updateUI(){
+    @objc func updateUI(){
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = .clear
+        falseButton.backgroundColor = .clear
+        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
     }
     
 }
